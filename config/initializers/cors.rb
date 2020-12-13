faee606b09a675797a7957b17fb171f01f15acea
raaynaldo <raaynaldo@hotmail.com>
2020-12-13 18:23:33 -0500

    Update cors

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -7,7 +7,7 @@
 
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins 'https://gitulize.netlify.app/'
+    origins 'gitulize.netlify.app'
 
     resource '*',
       headers: :any,
